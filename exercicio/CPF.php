b69d8e1e8112865d1cab816a1229d4d740915703
Diogo Almeida <diogoalmeidaealmeida@gmail.com>
2026-07-29 21:13:03 -0300

    Remove unused files

--- exercicio/CPF.php
+++
@@ -1,4 +0,0 @@
-<?php
-$lista=["094"=>"Geraldo","985"=>"Breno aura","837"=>"Pedrinho matador"];
-$valor=readline("Digite o CPF: ");
-echo $lista[$valor];
--- exercicio/looops.php
+++
@@ -1,9 +0,0 @@
-<?php
-$max=readline("Digite quantas tabúadas deseja: ");
-for ($i=1;$i<=$max;$i++) {
- for ($j=1;$j<=$max;$j++) {
-    $result=$i*$j;
-    echo "$i vezes $j: ";
-    echo "$result\n";
- }
-}
